f950fe0479d042db498a14811755b2da4f373b8c
Tom Molesworth <tom@perlsite.co.uk>
2013-09-15 01:54:21 +0000

    Prepare for CPAN release
    git-svn-id: http://entitymodel.com/repo/trunk/Tickit/Tickit-Widget-Table-Paged@466 c09ddf91-2ef0-45e8-8c68-47ccf8e3121d

--- cpanfile
+++ cpanfile
@@ -1,13 +1,7 @@
 requires 'parent', 0;
-requires 'curry', 0;
-requires 'Future', '>= 0.15';
-requires 'Try::Tiny', 0;
-requires 'Mixin::Event::Dispatch', '>= 1.000';
 requires 'Tickit', '>= 0.37';
 
 on 'test' => sub {
 	requires 'Test::More', '>= 0.98';
-	requires 'Test::Fatal', '>= 0.010';
-	requires 'Test::Refcount', '>= 0.07';
 };
 
--- lib/Tickit/Widget/Table/Paged.pm
+++ lib/Tickit/Widget/Table/Paged.pm
@@ -1,5 +1,5 @@
 package Tickit::Widget::Table::Paged;
-# ABSTRACT: 
+# ABSTRACT: a table widget for larger datasets
 use strict;
 use warnings;
 use parent qw(Tickit::Widget);
@@ -12,7 +12,35 @@ Tickit::Widget::Table::Paged - table widget with support for scrolling/paging
 
 =head1 SYNOPSIS
 
+ use Tickit;
+ use Tickit::Widget::Table::Paged;
+
+ my $tbl = Tickit::Widget::Table::Paged->new;
+ $tbl->{row_offset} = 0;
+ $tbl->add_column(
+ 	label => 'Left',
+ 	align => 'left',
+ 	width => 8,
+ );
+ $tbl->add_column(
+ 	label => 'Second column',
+ 	align => 'centre'
+ );
+ $tbl->add_row('left', 'middle') for 1..100;
+ Tickit->new(root => $tbl)->run;
+
 =head1 DESCRIPTION
+
+B<WARNING>: This is a preview release. API is subject to change in future,
+please get in contact if you're using this, or wait for version 1.000.
+
+This widget provides a scrollable table implementation.
+
+=begin HTML
+
+<p><img src="http://tickit.perlsite.co.uk/cpan-screenshot/tickit-widget-table-paged1.gif" alt="Paged table widget in action" width="430" height="306"></p>
+
+=end HTML
 
 =cut
 
@@ -59,26 +87,75 @@ BEGIN {
 
 =cut
 
+=head2 new
+
+Instantiate. Will attempt to take focus.
+
+=cut
+
+sub new {
+	my $class = shift;
+	my $self = $class->SUPER::new(@_);
+	$self->take_focus;
+	$self
+}
+
+=head2 lines
+
+Number of lines to request.
+
+=cut
+
 sub lines { 1 }
+
+=head2 cols
+
+Number of columns to request.
+
+=cut
+
 sub cols { 1 }
 
-=pod
+=head2 vscroll
 
-align:
-* left => 0
-* center|centre|middle => 0.5
-* right => 1
+True if there's a vertical scrollbar.
 
 =cut
 
 sub vscroll { 1 }
+
+=head2 hscroll
+
+True if there's a horizontal scrollbar.
+
+=cut
+
 sub hscroll { 0 }
+
+=head2 row_offset
+
+Current row offset (vertical scroll position).
+
+=cut
+
 sub row_offset { shift->{row_offset} ||= 0 }
+
+=head2 visible_lines
+
+Returns the list of lines currently visible in the display.
+
+=cut
 
 sub visible_lines {
 	my $self = shift;
 	return @{$self->{data}}[$self->row_offset..$self->row_offset + $self->scroll_dimension];
 }
+
+=head2 header_rect
+
+Returns the L<Tickit::Rect> representing the header area.
+
+=cut
 
 sub header_rect {
 	my $self = shift;
@@ -87,12 +164,24 @@ sub header_rect {
 	);
 }
 
+=head2 body_rect
+
+Returns the L<Tickit::Rect> representing the body area.
+
+=cut
+
 sub body_rect {
 	my $self = shift;
 	$self->{body_rect} ||= Tickit::Rect->new(
 		top => 1, lines => $self->window->lines - 1, left => 0, cols => $self->window->cols - 1
 	);
 }
+
+=head2 scrollbar_rect
+
+Returns the L<Tickit::Rect> representing the scroll bar.
+
+=cut
 
 sub scrollbar_rect {
 	my $self = shift;
@@ -101,9 +190,14 @@ sub scrollbar_rect {
 	);
 }
 
+=head2 render_header
+
+Render the header area.
+
+=cut
+
 sub render_header {
-	my ($self, $rb, $rect) = @_;
-	my $win = $self->window;
+	my ($self, $rb) = @_;
 	$rb->goto(0, 0);
 	for (@{$self->{column_layout}}) {
 		if(($_->{type} // '') eq 'padding') {
@@ -117,6 +211,12 @@ sub render_header {
 	$rb->text(' ', $self->get_style_pen('header')) if $self->vscroll;
 }
 
+=head2 render_to_rb
+
+Render the table.
+
+=cut
+
 sub render_to_rb {
 	my ($self, $rb, $rect) = @_;
 	my $win = $self->window;
@@ -129,6 +229,12 @@ sub render_to_rb {
 	$win->cursor_at($highlight_pos, 0);
 }
 
+=head2 render_body
+
+Render the body area.
+
+=cut
+
 sub render_body {
 	my ($self, $rb, $rect) = @_;
 	return $self unless my $win = $self->window;
@@ -138,7 +244,11 @@ sub render_body {
 	for my $y ($rect->linerange) {
 		next LINE unless $y;
 		next LINE if $y >= @visible;
-		next LINE unless $visible[$y];
+		unless($visible[$y]) {
+			$rb->goto($y, $rect->left);
+			$rb->text(' ' x $rect->cols, $self->get_style_pen('padding'));
+			next LINE;
+		}
 		$rb->goto($y, 0);
 		my @col_text = @{$visible[$y]};
 		CELL:
@@ -162,6 +272,12 @@ sub render_body {
 	}
 }
 
+=head2 apply_column_widget
+
+Add widgets for the column.
+
+=cut
+
 sub apply_column_widget {
 	my $self = shift;
 	return $self unless my $win = $self->window;
@@ -177,8 +293,14 @@ sub apply_column_widget {
 	$self;
 }
 
+=head2 render_scrollbar
+
+Render the scrollbar.
+
+=cut
+
 sub render_scrollbar {
-	my ($self, $rb, $rect) = @_;
+	my ($self, $rb) = @_;
 	my $win = $self->window;
 	my $cols = $win->cols;
 	--$cols if $self->vscroll;
@@ -194,6 +316,12 @@ sub render_scrollbar {
 		$rb->vline_at(1, $h, $cols, LINE_SINGLE, $self->get_style_pen('scrollbar'), CAP_BOTH);
 	}
 }
+
+=head2 render_cell
+
+Render a given cell.
+
+=cut
 
 sub render_cell {
 	my $self = shift;
@@ -212,6 +340,12 @@ sub render_cell {
 	return $self;
 }
 
+=head2 reshape
+
+Handle reshape requests.
+
+=cut
+
 sub reshape {
 	my $self = shift;
 	delete @{$self}{qw(body_rect header_rect scrollbar_rect)};
@@ -219,6 +353,12 @@ sub reshape {
 	$self->distribute_columns;
 	$self->apply_column_widget;
 }
+
+=head2 distribute_columns
+
+Distribute space between columns.
+
+=cut
 
 sub distribute_columns {
 	my $self = shift;
@@ -246,6 +386,12 @@ sub distribute_columns {
 	$self
 }
 
+=head2 window_gained
+
+Called when a window has been assigned to the widget.
+
+=cut
+
 sub window_gained {
 	my $self = shift;
 	$self->SUPER::window_gained(@_);
@@ -255,6 +401,12 @@ sub window_gained {
 #	$self->distribute_columns;
 #	$self->apply_column_widget;
 }
+
+=head2 expose_rows
+
+Expose the given rows.
+
+=cut
 
 sub expose_rows {
 	my $self = shift;
@@ -268,20 +420,31 @@ sub expose_rows {
 	), @_;
 }
 
+=head2 highlight_row
+
+Returns the index of the currently-highlighted row.
+
+=cut
+
 sub highlight_row {
 	my $self = shift;
 	return $self->{highlight_row};
 }
+
+=head2 highlight_visible_row
+
+Returns the position of the highlighted row taking scrollbar into account.
+
+=cut
 
 sub highlight_visible_row {
 	my $self = shift;
 	return $self->{highlight_row} - $self->row_offset;
 }
 
-=pod
+=head2 key_previous_row
 
-row_offset - number of rows protruding off the top of the screen
-
+Go to the previous row.
 
 =cut
 
@@ -293,6 +456,12 @@ sub key_previous_row {
 	return $self->move_highlight(1) if $self->highlight_visible_row >= 1;
 	return $self->scroll_highlight(1);
 }
+
+=head2 scroll_highlight
+
+Update scroll information after changing highlight position.
+
+=cut
 
 sub scroll_highlight {
 	my $self = shift;
@@ -315,6 +484,12 @@ sub scroll_highlight {
 	$win->expose($_) for map $_->translate($up ? 1 : -1, 0), $redraw_rect->rects;
 }
 
+=head2 move_highlight
+
+Change the highlighted row.
+
+=cut
+
 sub move_highlight {
 	my $self = shift;
 	my $up = shift;
@@ -329,6 +504,12 @@ sub move_highlight {
 	$self
 }
 
+=head2 key_next_row
+
+Move to the next row.
+
+=cut
+
 sub key_next_row {
 	my $self = shift;
 	return $self unless my $win = $self->window;
@@ -338,6 +519,12 @@ sub key_next_row {
 	return $self->scroll_highlight(0);
 }
 
+=head2 key_first_row
+
+Move to the first row.
+
+=cut
+
 sub key_first_row {
 	my $self = shift;
 	$self->{highlight_row} = 0;
@@ -345,12 +532,24 @@ sub key_first_row {
 	$self->redraw;
 }
 
+=head2 key_last_row
+
+Move to the last row.
+
+=cut
+
 sub key_last_row {
 	my $self = shift;
 	$self->{highlight_row} = $self->row_count - 1;
 	$self->{row_offset} = $self->row_count > $self->scroll_dimension ? -1 + $self->row_count - $self->scroll_dimension : 0;
 	$self->redraw;
 }
+
+=head2 key_previous_page
+
+Go up a page.
+
+=cut
 
 sub key_previous_page {
 	my $self = shift;
@@ -360,6 +559,12 @@ sub key_previous_page {
 	$self->{row_offset} = 0 if $self->{row_offset} < 0;
 	$self->redraw;
 }
+
+=head2 key_next_page
+
+Go down a page.
+
+=cut
 
 sub key_next_page {
 	my $self = shift;
@@ -371,15 +576,41 @@ sub key_next_page {
 	$self->redraw;
 }
 
+=head2 scroll_position
+
+Current vertical scrollbar position.
+
+=cut
+
 sub scroll_position { shift->{row_offset} }
+
+=head2 row_count
+
+Total number of rows.
+
+=cut
+
 sub row_count { scalar @{shift->{data}} }
+
+=head2 sb_height
+
+Current scrollbar height.
+
+=cut
 
 sub sb_height {
 	my $self = shift;
 	my $ext = $self->scroll_dimension;
 	my $max = $self->row_count - $ext;
+	return 1 unless $max;
 	return floor(0.5 + ($ext * $ext / $max));
 }
+
+=head2 scroll_rows
+
+Positions of the scrollbar indicator.
+
+=cut
 
 sub scroll_rows {
 	my $self = shift;
@@ -387,9 +618,16 @@ sub scroll_rows {
 	my $ext = $self->scroll_dimension;
 	my $max = $self->row_count - $ext;
 	return if $max < $ext;
+	return unless $max;
 	my $y = floor(0.5 + ($cur * ($ext - $self->sb_height) / $max));
 	return $y, $y + $self->sb_height;
 }
+
+=head2 active_scrollbar_rect
+
+Rectangle representing the area covered by the current scrollbar.
+
+=cut
 
 sub active_scrollbar_rect {
 	my $self = shift;
@@ -402,11 +640,23 @@ sub active_scrollbar_rect {
 	);
 }
 
+=head2 scroll_dimension
+
+Size of the vertical scrollbar.
+
+=cut
+
 sub scroll_dimension {
 	my $self = shift;
 	return 1 unless my $win = $self->window;
 	$win->lines - 2;
 }
+
+=head2 clear
+
+Clear all data in the table.
+
+=cut
 
 sub clear {
 	my $self = shift;
@@ -415,6 +665,12 @@ sub clear {
 	$self->redraw;
 	$self
 }
+
+=head2 add_row
+
+Add a row to the table.
+
+=cut
 
 sub add_row {
 	my $self = shift;
@@ -430,6 +686,12 @@ my %ALIGNMENT_TYPE = (
 	middle => 0.5,
 );
 
+=head2 add_column
+
+Add a new column. Takes some parameters.
+
+=cut
+
 sub add_column {
 	my $self = shift;
 	my %args = @_;
@@ -442,16 +704,46 @@ sub add_column {
 	$self
 }
 
+=head2 key_next_column
+
+Move to the next column.
+
+=cut
+
 sub key_next_column { }
+
+=head2 key_previous_column
+
+Move to the previous column.
+
+=cut
+
 sub key_previous_column { }
+
+=head2 key_first_column
+
+Move to the first column.
+
+=cut
+
 sub key_first_column { }
+
+=head2 key_last_column
+
+Move to the last column.
+
+=cut
+
 sub key_last_column { }
 
-sub on_key { warn "on_key with @_\n"; 0 }
+=head2 key_activate
+
+Activate the highlighted item.
+
+=cut
 
 sub key_activate {
 	my $self = shift;
-	warn "activate!";
 	$self->{on_activate}->(
 		$self->highlight_row,
 		$self->{data}[$self->highlight_row]
@@ -463,13 +755,37 @@ sub key_activate {
 
 __END__
 
+=head1 TODO
+
+Current list of pending features:
+
+=over 4
+
+=item * Storage abstraction - the main difference between this widget and
+L<Tickit::Widget::Table> is that this is designed to work with a storage
+abstraction. The current abstraction implementation needs more work before
+it's reliable enough for release, so this version only has basic arrayref
+support.
+
+=item * Column and cell highlighting modes
+
+=item * Multiselection
+
+=item * Proper widget-in-cell support
+
+=item * Formatters for converting raw cell data into printable format
+(without having to go through a separate widget)
+
+=back
+
 =head1 SEE ALSO
 
 =over 4
 
-=item * L<Tickit::Widget::Table>
+=item * L<Tickit::Widget::Table> - older table implementation based on L<Tickit::Widget::HBox> and L<Tickit::Widget::VBox>
+widgets. Does not support scrolling and performance isn't as good, so it will eventually be merged with this one.
 
-=item * L<Text::ANSITable>
+=item * L<Text::ANSITable> - not part of L<Tickit> but has some impressive styling capabilities.
 
 =back
 
